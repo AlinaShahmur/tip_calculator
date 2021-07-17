d4498f54d0fc2cdd99a51b05afadbcb8ee3cffaf
AlinaShahmur <ashaturnaya@gmail.com>
2021-07-17 19:00:47 +0300

    changed the file structure

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8000;
+const appPath = path.join(__dirname, '..', 'build')
+
+
+app.use(express.static(appPath))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(appPath, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Serve is up on port ${port}`);
+})
